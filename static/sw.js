8399e14f2a573377b586d9ab0ea0ebf1741c69da
flexDr <101925304+flexDr@users.noreply.github.com>
2026-06-02 16:41:02 -0400

    Update and rename service-worker.js to sw.js

---
+++ static/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'musicapi_cache_v1';
+const URLS_A_CACHEAR = [
+  '/',
+  '/static/index.html',
+  '/static/app.js',
+  '/manifest.json'
+];
+
+// Fase 1: Instalación (Guardar la interfaz en el celular)
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_A_CACHEAR))
+  );
+  self.skipWaiting();
+});
+
+// Fase 2: Intercepción de red
+self.addEventListener('fetch', e => {
+  // REGLA DE ORO: Nunca cachear el túnel de audio. 
+  // Si cacheamos la música, el iPhone colapsará la memoria RAM.
+  if (e.request.url.includes('/api/stream')) {
+      return; 
+  }
+  
+  // Para todo lo demás (HTML, CSS, JS), sirve la versión ultrarrápida del caché
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
